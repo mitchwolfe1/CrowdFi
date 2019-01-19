0ee913b1fec17a96b2d29e77adaedc6264282ac1
vsftpd <x0rzinq@gmail.com>
2019-01-19 00:16:35 -0800

    parsing update

--- stream.py
+++ stream.py
@@ -2,6 +2,5 @@ import subprocess
 import sys
 
 process = subprocess.Popen(["sh", "lol.sh"], stdout=subprocess.PIPE)
-for line in iter(process.stdout.readline, ''):
-#	sys.stdout.write(line)
-	print(line)
+for line in iter(process.stdout.readline, b''):
+	print(str(line))
