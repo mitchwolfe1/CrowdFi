cd2ac80e5a6dfe47320d20dc35ba2f78c36be7ec
mitch <vsftpd>
2019-01-19 11:46:43 -0800

    start.js socket

--- pi-socket/start.js
+++ pi-socket/start.js
@@ -1,4 +1,6 @@
 const ServerSock = require('./serversock.js');
-
+const MapsSock = require('./mapssock.js')
 var sock = new ServerSock(6969);
+var mapssock = new MapsSock(1337);
 sock.startWebSocket();
+mapssock.startWebSocket();
