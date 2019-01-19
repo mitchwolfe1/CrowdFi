ca3b7bce6286369ef797f439c6681faa188a4b1f
Kevin <kevin12kim27@gmail.com>
2019-01-18 23:04:10 -0800

    Added signal testing java file

---
+++ SignalTest.java
@@ -0,0 +1,22 @@
+import java.math.*;
+import java.util.Scanner;
+public class SignalTest {
+	public static void main(String[] args)
+	{
+		Scanner s;
+    double strength;
+    double frequency;
+
+    s = new Scanner(System.in);
+    System.out.println("Enter signal strength (MHz): ");
+		frequency = 2400;
+    System.out.println(getDistance(strength, frequency));
+
+
+
+  public double getDistance(double signalStrengthMHz, double frequency)
+  {
+    return Math.pow(10.0, 	double exp = (27.55 - (20 * Math.log10(frequency)) + Math.abs(strength)) / 20.0);
+  }
+
+}
