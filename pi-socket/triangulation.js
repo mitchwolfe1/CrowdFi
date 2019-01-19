9ca06fb7561ff97d5d4594f801f9063fbdf697b8
root <root@instance-1.us-west1-b.c.cybernetic-apex-229104.internal>
2019-01-19 17:55:43 +0000

    Work on triangulation because kevin was sleeping

--- pi-socket/triangulation.js
+++ pi-socket/triangulation.js
@@ -1,6 +1,49 @@
 const redis = require('redis'),
 	client = redis.createClient();
 
+class Point {
+	constructor(x, y, z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	static distance(p1, p2) {
+		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2) + Math.pow(p1.z - p2.z, 2));
+
+	}
+
+	static add(p1, p2) {
+		return new Point(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z);
+	}
+
+	static sub(p1, p2) {
+		return new Point(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
+	}
+
+	static mult(p1, mul) {
+		return new Point(p1.x * mul, p1.y * mul, p1.z * mul);
+	}
+
+
+
+	distance(aPoint) {
+		return Point.distance(this, aPoint);
+	}
+
+	add(aPoint) {
+		return Point.add(this, aPoint);
+	}
+
+	sub(aPoint) {
+		return Point.sub(this, aPoint);
+	}
+
+	mult(mul) {
+		return Point.mult(this, mul);
+	}
+}
+
 class Triangulation {
 	constructor() {}
 
@@ -21,9 +64,7 @@ class Triangulation {
 
 	triangulateDevice(device) {
 		// Gather the necessary data from redis
-		var signal_strengths = [];
-		var rpiInfo = [];
-		var getSignalStrengthPromise = new Promise(function(resolve, reject) {
+		var getDistancesPromise = new Promise(function(resolve, reject) {
 			client.hmget(device, "1", "2", "3", "4", "5", function (err, res) {
 				if (err != null) {
 					reject(err);
@@ -31,10 +72,40 @@ class Triangulation {
 					resolve(res);
 				}
 			});
-		}).then(function(result) {}
-		);
-	}
+		});
+		getDistancesPromise.then(function(result) {
+			let rpiPromises = [];
+			let rpiIds = ["PI1", "PI2", "PI3", "PI4", "PI5"];
+			for (let i in rpiIds) {
+				let rpiId = rpiIds[i];
+				console.log("ID: " + rpiId);
+				let rpiPromise = new Promise(function (resolve, reject) {
+					client.hmget(rpiId, "lat", "long", function (err, res) {
+						if (err != null) {
+							reject(err);
+						} else {
+							resolve(res);
+						}
+					});
 
+				});
+				rpiPromises.push(rpiPromise);
+			}
+
+			Promise.all(rpiPromises).then(function(values) {
+				let ITER = 2000;
+				let ALPHA = 2.0;
+				let RATIO = 0.99;
+				let piData = {"PI1" : values[0], "PI2" : values[1], "PI3" : values[2], "PI4" : values[3], "PI5" :  values[4]};
+				let distances = result;
+				console.log(piData);
+				console.log(distances);
+			});
+		});
+		getDistancesPromise.catch(function(error) {
+			console.log(error);
+		});
+	}
 }
 
 
