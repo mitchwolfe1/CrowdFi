a8098f507b5446e373cda98e1b85afa699844acd
mitch <vsftpd>
2019-01-19 01:05:49 -0800

    added cli

--- lol.sh
+++ lol.sh
@@ -1,1 +1,2 @@
-tcpdump -l -I -i "wlan1" -e -s 256 type mgt subtype probe-req | awk -f parse-tcpdump.awk | tee -a "out.txt"
+echo "$1";
+tcpdump -l -I -i "$1" -e -s 256 type mgt subtype probe-req | awk -f parse-tcpdump.awk | tee -a "out.txt";
--- stream.py
+++ stream.py
@@ -1,6 +1,27 @@
 import subprocess
 import sys
+import netifaces
 
-process = subprocess.Popen(["sh", "lol.sh"], stdout=subprocess.PIPE)
+scan_iface = sys.argv[1]
+mac_iface = sys.argv[2]
+
+process = subprocess.Popen(["sh", "lol.sh", scan_iface], stdout=subprocess.PIPE)
 for line in iter(process.stdout.readline, b''):
-	print(str(line))
+	try:
+		line = str(line).replace("b'", "").replace("\\n'", "")
+		line_arr = line.split(" ")
+		ts = line_arr[0]
+		signal_strength = int(line_arr[1].replace("dBm", ""))
+		mac_addy = line_arr[2]
+		ssid = line_arr[3].replace("\"", "")
+		rpi_mac = netifaces.ifaddresses(mac_iface)[netifaces.AF_LINK][0]["addr"]
+	except Exception as e:
+		ts = ""
+		signal_strength = ""
+		mac_addy = ""
+		ssid = ""
+		rpi_mac = ""
+
+		print(str(e))
+
+	print(ts + " " + str(signal_strength) + " " + mac_addy + " " + ssid + " " + rpi_mac)
